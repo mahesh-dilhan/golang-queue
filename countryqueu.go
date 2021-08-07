7f800d23ae84c63470691dfdfa2b022216574bb3
mahesh <mail.mahesh.lk@gmail.com>
2021-08-07 19:01:42 +0800

    heap-prority - queue

--- countryqueu.go
+++ countryqueu.go
@@ -1,6 +1,8 @@
 package queue
 
 //priority queue impl
+//since we use heap
+// sort interface required
 type CountryQueue []*Country
 
 func (cq *CountryQueue) Push(patient interface{}) {
@@ -9,4 +11,8 @@ func (cq *CountryQueue) Push(patient interface{}) {
 
 func (cq *CountrySeverity) Pop() interface{} {
 	return nil
+}
+
+func (cq *CountryQueue) Len() int {
+	return len(*cq)
 }
