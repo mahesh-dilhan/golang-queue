c8e09e338ac7bd9c964f416e7fff5bd8a80dfec8
mahesh <mail.mahesh.lk@gmail.com>
2021-08-07 19:09:08 +0800

    heap-prority - queue

--- countryqueu.go
+++ countryqueu.go
@@ -22,3 +22,8 @@ func (cq CountryQueue) swap(a, b int) {
 	cq[a].index = a
 	cq[b].index = b
 }
+
+//We need to use map heap based on severity
+func (cq CountryQueue) Less(a, b int) bool {
+	return cq[a].severity > cq[b].severity
+}
