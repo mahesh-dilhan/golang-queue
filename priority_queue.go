95d6587e481dc67b1311e3e1301a3aa19acd384f
mahesh <mail.mahesh.lk@gmail.com>
2021-08-07 18:59:10 +0800

    prority - queue

--- priority_queue.go
+++ priority_queue.go
@@ -1,9 +1,12 @@
 package queue
 
+import "container/heap"
+
 type EmergencyCountry struct {
 	countryQueue CountryQueue
 }
 
 func NewEmergencyCountry() *EmergencyCountry {
 	err := &EmergencyCountry{countryQueue: make(CountryQueue, 0)}
+	heap.Init()
 }
