f9e9d793ae0d839ab099efaa2970a162557b3cc3
mahesh <mail.mahesh.lk@gmail.com>
2021-08-07 18:58:04 +0800

    prority - queue

--- priority_queue.go
+++ priority_queue.go
@@ -3,3 +3,7 @@ package queue
 type EmergencyCountry struct {
 	countryQueue CountryQueue
 }
+
+func NewEmergencyCountry() *EmergencyCountry {
+	err := &EmergencyCountry{countryQueue: make(CountryQueue, 0)}
+}
