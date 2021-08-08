8ad874cc25003cf6f678f30664af0e418284d8c7
mahesh <mail.mahesh.lk@gmail.com>
2021-08-08 18:59:06 +0800

    nested

--- nested/main.go
+++ nested/main.go
@@ -1,1 +1,31 @@
-package nested
+package main
+
+import "fmt"
+
+type Contact struct {
+	phone string
+}
+
+type Person struct {
+	name    string
+	contact Contact
+}
+
+func (c *Contact) changeContact(newContact string) {
+	c.phone = newContact
+}
+func main() {
+	c := Contact{
+		phone: "10098387123",
+	}
+	p := Person{
+		name:    "Mahesh",
+		contact: c,
+	}
+
+	fmt.Println(p)
+
+	p.contact.changeContact("876123709123")
+
+	fmt.Println(p)
+}
