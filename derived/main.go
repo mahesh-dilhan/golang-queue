54989fa6fb9f4218c729195712eb239632030858
mahesh <mail.mahesh.lk@gmail.com>
2021-08-09 18:59:37 +0800

    nested

--- derived/main.go
+++ derived/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type DerivedType string
+
+func (d *DerivedType) toUppercase(str string) string {
+	return strings.ToUpper(str)
+}
+
+func main() {
+	d := DerivedType("sg")
+	fmt.Println(d.toUppercase("sg"))
+}
