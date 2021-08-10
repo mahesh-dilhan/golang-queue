8e7380ebe65c782d1d46daca1c1a5073fdbffa91
mahesh <mail.mahesh.lk@gmail.com>
2021-08-10 19:26:23 +0800

    nested

--- error/main.go
+++ error/main.go
@@ -1,1 +1,13 @@
-package error
+package main
+
+type MyError struct {
+}
+
+func (er *MyError) Error() []rune {
+	merror := []rune{100, 100, 100}
+	return merror
+}
+
+func main() {
+	e := MyError{}
+}
